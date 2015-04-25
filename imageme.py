32b4ee950ebe8f8b11c063f584395aab7abb264e
groar <alois@smyle.io>
2015-04-25 17:37:46 +0200

    order.pkl

--- imageme.py
+++ imageme.py
@@ -305,7 +305,9 @@ def _create_index_files(root_dir, force_no_processing=False):
 
         order = None
         if (os.path.exists(os.path.join(here, "order.pkl"))):
+        	print "hello"
         	order = joblib.load(os.path.join(here, "order.pkl"))
+        	print order[0:10]
         	images_files = map(lambda x : x[0], order)
 
         # Create this directory's index file and add its name to the created
